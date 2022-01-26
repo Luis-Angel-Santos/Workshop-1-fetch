482aef088766068b20187a47d7f0f3d87411d3b8
Luis Angel Santos <santosangel748@gmail.com>
2022-01-25 18:39:44 -0600

    Dando un poco de estilos

--- src/index.js
+++ src/index.js
@@ -28,20 +28,27 @@ window
             //crear imagen, titulo, precio
             const imagen = document.createElement('img');
             imagen.src = `${BaseUrl}${item.image}`;
+            imagen.className = "h-40 w-40 md:h-40 md:w-40 rounded-full mx-auto ";
 
             const titulo = document.createElement('h2');
             titulo.textContent = item.name;
+            titulo.className = 'text-center';
             titulo.style.fontSize = '1rem';
 
             const precio = document.createElement('div');
             precio.textContent = formatPrice(item.price);
-            precio.style.color = 'darkgray';
+            precio.style.color = 'dark';
+            precio.style.backgroundColor = 'yellow'
+            precio.className = 'text-center rounded-lg p-1 w-20 mx-auto'
 
             const container = document.createElement('div');
-            document.body.append(imagen, titulo, precio);
+            container.append(imagen, titulo, precio);
+            container.className = 'rounded-lg p-6 m-2 hover:bg-gray-300'
+            
 
             todoslosItems.push(container);
         });
         appNode.append(...todoslosItems);
+       //appNode.className = 'md:flex'
     });
 
